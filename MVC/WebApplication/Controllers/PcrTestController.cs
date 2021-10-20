f601264cfaa998dece5d12c51b5be8a53f15e888
Ludwig Lebrun <lle@eonix.be>
2021-10-20 21:28:06 +0200

    full CRUD

--- MVC/WebApplication/Controllers/PcrTestController.cs
+++ MVC/WebApplication/Controllers/PcrTestController.cs
@@ -1,33 +1,91 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApplication.Entities;
 using WebApplication.Models;
+using WebApplication.Persistence;
 
 namespace WebApplication.Controllers
 {
     public class PcrTestController : Controller
     {
         private readonly ILogger<PcrTestController> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-        public PcrTestController(ILogger<PcrTestController> logger)
+        public PcrTestController(ILogger<PcrTestController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         // GET
         public IActionResult Index()
         {
-            return View();
+            return View(_dbContext.PcrTests.ToList());
         }
 
         public IActionResult Create()
         {
-            return View();
+            return RedirectToAction("Edit");
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            PcrTest pcrTest = null;
+
+            if (id != Guid.Empty)
+                pcrTest = _dbContext.PcrTests.Find(id);
+
+            pcrTest ??= new PcrTest();
+            
+            return View(new PcrTestViewModel()
+            {
+                Code = pcrTest.Code,
+                Comment = pcrTest.Comment,
+                AnalysisDate = pcrTest.AnalysisDate,
+                Id = pcrTest.Id,
+            });
         }
 
         [HttpPost]
-        public IActionResult SavePcrTest(PcrTestViewModel model)
+        public IActionResult Edit(PcrTestViewModel model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+
             _logger.LogTrace($"Received model with code {model.Code}");
+
+            PcrTest pcrTest = null;
+
+            if (model.Id != Guid.Empty)
+                pcrTest = _dbContext.PcrTests.Find(model.Id);
+
+            pcrTest ??= new PcrTest();
+            pcrTest.Code = model.Code;
+            pcrTest.Comment = model.Comment;
+            pcrTest.AnalysisDate = model.AnalysisDate;
+            pcrTest.ReceptionDate = model.ReceptionDate;
+
+            _dbContext.Update(pcrTest);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Delete(Guid id)
+        {
+            PcrTest pcrTest = null;
+
+            if (id != Guid.Empty)
+                pcrTest = _dbContext.PcrTests.Find(id);
+
+            if (pcrTest != null)
+            {
+                _dbContext.Remove(pcrTest);
+                _dbContext.SaveChanges();
+            }
+
             return RedirectToAction("Index");
         }
     }
--- MVC/WebApplication/Models/PcrTestViewModel.cs
+++ MVC/WebApplication/Models/PcrTestViewModel.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication.Models
 {
     public class PcrTestViewModel
     {
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Le champ 'Code' est requis.")]
+        [StringLength(8, ErrorMessage = "Code length can't be more than 8.")]
         public string Code { get; set; }
         public string Comment { get; set; }
-        public DateTime ReceptionDate { get; set; }
-        public DateTime AnalysisDate { get; set; }
+        public DateTime? ReceptionDate { get; set; }
+        public DateTime? AnalysisDate { get; set; }
     }
 }
