9efc10fc6f52a1e54c11fca524fa11d779f4e585
Ludwig Lebrun <lle@eonix.be>
2021-11-16 21:15:08 +0100

    AJAX

---
+++ MVC/WebApplication/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly ILogger<MessageController> _logger;
+
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public PartialViewResult GetMessageAlert(string message = "")
+        {
+            return PartialView("_Message", message ?? Guid.NewGuid().ToString());
+        }
+    }
+}
