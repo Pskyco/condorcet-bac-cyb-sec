036b80a2a7a3785253df8764cb18213fbb97c37a
Ludwig Lebrun <lle@eonix.be>
2022-02-23 18:48:40 +0100

    Swagger: add authorization code flow & client credentials flow

--- CyberSecurity/IS4Samples/MyApi/Program.cs
+++ CyberSecurity/IS4Samples/MyApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +25,78 @@ builder.Services.AddAuthorization(options => options.AddPolicy("MyApiScope", pol
 }));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My Protected API", Version = "v1" });
+
+    var oauth2SecurityScheme = new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Scheme = "Bearer",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Flows = new OpenApiOAuthFlows()
+        {
+            AuthorizationCode = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri("https://localhost:5001/connect/authorize"),
+                TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                Scopes = new Dictionary<string, string>()
+                {
+                    { "myApi", "My API - Full access" }
+                }
+            }
+        }
+    };
+    options.AddSecurityDefinition("oauth2", oauth2SecurityScheme);
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme, 
+                    Id = "oauth2"
+                }
+            },
+            new[] { "openid", "profile", "myApi" }
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Scheme = "Bearer",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        BearerFormat = "JWT",
+        Flows = new OpenApiOAuthFlows
+        {
+            ClientCredentials = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri("https://localhost:5001/connect/token")
+            }
+        },
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Name = "Bearer"
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -32,7 +104,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+        options.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
